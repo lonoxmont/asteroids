836f585bc83241d5e386c0ecd8a7e2de4bccee2e
lonoxmont <lonoxmont@suptg.com>
2024-12-12 13:23:43 -0800

    added player object to the game

--- main.py
+++ main.py
@@ -3,12 +3,14 @@
 # throughout this file
 import pygame
 from constants import *
+import player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player1 = player.Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -19,6 +21,7 @@ def main():
         screen.fill((0, 0, 0))
         delay = clock.tick(60)
         dt = delay / 1000
+        player1.draw(screen)
         pygame.display.flip()
 
 if __name__ == "__main__":
